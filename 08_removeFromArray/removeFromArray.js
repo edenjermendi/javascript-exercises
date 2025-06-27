8f98c7a513925377519b73fbbbe56c829c539a1c
edenjermendi <ejermendi@gmail.com>
2025-06-27 15:08:29 +1200

    Complete exercises 6, 7 and 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let value of args) {
+        let index = array.indexOf(value);
+        while (array.indexOf(value) !== -1) {
+        array.splice(array.indexOf(value), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
