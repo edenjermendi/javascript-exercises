c06ada9ba63286dbfbbddfd3c138c5bf2cacc8dd
edenjermendi <ejermendi@gmail.com>
2025-06-29 17:15:20 +1200

    Exercise 13 complete Look at prior code to find reverse string Use regex to clean up string for remaining tests All tests passed!

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
+
