5e23ec6a24984daeadd58f6997d2c22e348cd434
edenjermendi <ejermendi@gmail.com>
2025-06-29 18:34:09 +1200

    fix: add return 0 for fibonacci(0) fix: support string inputs in fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,7 +1,12 @@
 const fibonacci = function(n) {
-    if (n < 1) {
+    n = Number(n);
+
+    if(n === 0) {
+        return 0;
+    } else if (n < 1) {
     return "OOPS"
     }
+    
     let fib =  [1, 1];
     for (let i = 2; i < n; i++) {
       fib[i] = fib [i - 1] + fib[i - 2];
