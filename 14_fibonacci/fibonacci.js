3732a8989a7b2cedce10409284b573492b3b0f06
edenjermendi <ejermendi@gmail.com>
2025-06-29 18:18:30 +1200

    Use AI to help figure out fibonacci Use for loop inside func to repeat pattern Declare arr of 1, 1 so 0 and 1 index are both 1
    Testing broke when I add 'if' statement -
    Troubleshooting this now

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 1) {
+    return "OOPS"
+    }
+    let fib =  [1, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib [i - 1] + fib[i - 2];
+    }
+    return fib[n - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
