71296570b12ec77d736de593633c178cd7eaa8ad
edenjermendi <ejermendi@gmail.com>
2025-07-02 18:15:42 +1200

    feat: add getAge function for age calculation fix: check .date syntax on W3 schools chore: review problem reqs for findOldest exercise docs: research reduce method for array iteration

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -4,3 +4,9 @@ const findTheOldest = function() {
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+function getAge(person) {
+    const currentYear = new Date().getFullYear(); //grab current year
+    const yearOfDeath = person.yearOfDeath ?? currentYear; //if death use it otherwise current yr
+    return yearOfDeath - person.yearOfBirth; //subtract to give age
+};
