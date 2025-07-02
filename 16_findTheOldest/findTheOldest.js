6f2ced23bf4ff038dd4d32baa2b36e90ea9d8a39
edenjermendi <ejermendi@gmail.com>
2025-07-02 19:11:05 +1200

    feat: add findTheOldest with reduce and getAge docs: explain reduce and initial value use chore: complete tests for findTheOldest docs: note tutor GPT helped in solving problem

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,12 +1,21 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
-
 function getAge(person) {
     const currentYear = new Date().getFullYear(); //grab current year
     const yearOfDeath = person.yearOfDeath ?? currentYear; //if death use it otherwise current yr
     return yearOfDeath - person.yearOfBirth; //subtract to give age
 };
+
+function findTheOldest(people) {
+  return people.reduce((oldestSoFar, currentPerson) => {
+    const currentAge = getAge(currentPerson);
+    const oldestAge = getAge(oldestSoFar);
+
+    if (currentAge > oldestAge) {
+      return currentPerson;
+    } else {
+      return oldestSoFar;
+    }
+  }, people[0]);
+};
+
+// Do not edit below this line
+module.exports = findTheOldest;
